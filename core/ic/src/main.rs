ae027691f9768364232c6ddf556bfd8ca6482604
ccyanxyz <ccyanxyz@gmail.com>
2022-08-18 22:17:09 +0800

    fix

--- core/ic/src/main.rs
+++ core/ic/src/main.rs
@@ -21,9 +21,10 @@ use ic_web3::{
 };
 
 // goerli testnet rpc url
-const URL: &str = "https://goerli.infura.io/v3/93ca33aa55d147f08666ac82d7cc69fd";
+// const URL: &str = "https://goerli.infura.io/v3/93ca33aa55d147f08666ac82d7cc69fd";
+const URL: &str = "https://eth-goerli.g.alchemy.com/v2/0QCHDmgIEFRV48r1U1QbtOyFInib3ZAm";
 const CHAIN_ID: u64 = 5;
-const ETH_OMNIC_ADDR: &str = "0fA355bEEA41d190CAE64F24a58F70ff2912D7df";
+const ETH_OMNIC_ADDR: &str = "0fa355beea41d190cae64f24a58f70ff2912d7df";
 const EVENT_ENQUEUE_MSG: &str = "49855fe1b89449bbbf62ad50dd54754b7834260e96c7986a103cbcb95883353c";
 
 const KEY_NAME: &str = "dfx_test_key";
@@ -65,18 +66,19 @@ async fn get() {
 
 async fn get_logs() {
     ic_cdk::println!("getting logs now");
+    // ic_cdk::println!("{}", H160::from_str("25816551e0e2e6fc256a0e7bcffdfd1ca3cd390d").unwrap());
     // should traverse all chains, testing only ETH now
     // filter for events in our contract
     let filter = FilterBuilder::default()
-        .address(vec![serde_json::from_str::<H160>(ETH_OMNIC_ADDR).unwrap()])
+        .address(vec![H160::from_str(ETH_OMNIC_ADDR).unwrap()])
         .topics(
-            Some(vec![serde_json::from_str::<H256>(EVENT_ENQUEUE_MSG).unwrap()]),
+            Some(vec![H256::from_str(EVENT_ENQUEUE_MSG).unwrap()]),
             None,
             None,
             None,
         )
         .from_block(BlockNumber::Number(7426080.into())) // omnic contract deploy block id
-        .to_block(BlockNumber::Latest)
+        .to_block(BlockNumber::Number(7426080.into()))
         .build();
     let w3 = match ICHttp::new(URL, None) {
         Ok(v) => { Web3::new(v) },
@@ -93,9 +95,7 @@ fn heartbeat() {
     for task in cron_ready_tasks() {
         let kind = task.get_payload::<Task>().expect("Serialization error");
         match kind {
-            Task::GetLogs => {
-                ic_cdk::spawn(get_logs());
-            },
+            Task::GetLogs => todo!(),
             SendTx => todo!()
         }
     }
