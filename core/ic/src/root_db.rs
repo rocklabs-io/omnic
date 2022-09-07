d209bc7178c12801186965e1fb260b83e38485f8
ccyanxyz <ccyanxyz@gmail.com>
2022-09-07 18:58:17 +0800

    add proxy

---
+++ core/ic/src/root_db.rs
@@ -0,0 +1,38 @@
+// merkle root storage, for proxy canister
+
+use std::collections::VecDeque;
+use ic_web3::types::H256;
+
+use crate::types::{Message, RawMessage};
+use crate::error::OmnicError;
+
+
+pub struct RootDB {
+    pub roots: VecDeque<(H256, u64)>,
+    pub optimistic_delay: u64,
+}
+
+impl RootDB {
+    pub fn new() -> Self {
+        RootDB {
+            msgs: VecDeque::new(),
+            optimistic_delay: 1800, // 30 mins
+        }
+    }
+
+    pub fn insert_root(&mut self, root: H256, confirm_at: u64) {
+
+    }
+
+    // get the latest root
+    pub fn latest_root(&self) -> H256 {
+
+    }
+
+    // get the latest root that has passed the optimistic verification challenge period
+    // now - confirmAt >= 30 mins
+    pub fn latest_op_root(&self, now: u64) -> H256 {
+
+    }
+
+}
