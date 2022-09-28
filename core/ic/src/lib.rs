f0a6338d0692fbe201d1f90e6046cb8ab14f00ac
ccyanxyz <ccyanxyz@gmail.com>
2022-09-29 00:06:43 +0800

    cleanup

--- core/ic/src/lib.rs
+++ core/ic/src/lib.rs
@@ -4,14 +4,10 @@ pub mod utils;
 pub mod types;
 pub mod traits;
 pub mod chains;
-pub mod message_db;
 pub mod root_db;
-pub mod home;
 pub mod error;
 
 pub use types::*;
 pub use traits::*;
-pub use message_db::*;
 pub use root_db::*;
-pub use home::*;
 pub use error::*;
--- core/ic/src/traits/chain.rs
+++ core/ic/src/traits/chain.rs
@@ -7,15 +7,6 @@ use crate::error::OmnicError;
 
 
 #[async_trait]
-pub trait HomeIndexer {
-    async fn get_block_number(&self) -> Result<u32, OmnicError>;
-
-    /// Fetch list of messages between blocks `from` and `to`.
-    async fn fetch_sorted_messages(&self, _from: u32, _to: u32) -> Result<Vec<RawMessage>, OmnicError>;
-}
-
-
-#[async_trait]
 pub trait HomeContract {
     async fn send_message(&self, msg: &Message) -> Result<Option<H256>, OmnicError>;
     async fn get_latest_root(&self, height: Option<u32>) -> Result<H256, OmnicError>;
