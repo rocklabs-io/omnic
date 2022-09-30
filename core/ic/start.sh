557b6c964629e743d66ca7283a7b324c1532e888
ccyanxyz <ccyanxyz@gmail.com>
2022-09-30 16:37:53 +0800

    new deployment

--- core/ic/start.sh
+++ core/ic/start.sh
@@ -12,9 +12,9 @@ dfx build demo
 dfx canister install proxy
 dfx canister install demo
 
-dfx canister call proxy add_chain "(5:nat32, vec {\"https://eth-goerli.g.alchemy.com/v2/0QCHDmgIEFRV48r1U1QbtOyFInib3ZAm\"; \"https://goerli.infura.io/v3/93ca33aa55d147f08666ac82d7cc69fd\"}, \"0312504E22B40A6f03FcCFEA0C8c0e9Ad3E36918\", 7558863:nat64)"
+dfx canister call proxy add_chain "(5:nat32, vec {\"https://eth-goerli.g.alchemy.com/v2/0QCHDmgIEFRV48r1U1QbtOyFInib3ZAm\"; \"https://goerli.infura.io/v3/93ca33aa55d147f08666ac82d7cc69fd\"}, \"22216796e65F3C786d853F818ca1fc0f661639C0\", 7685333:nat64)"
 
-dfx canister call proxy add_chain "(80001:nat32, vec{\"https://polygon-mumbai.g.alchemy.com/v2/0QCHDmgIEFRV48r1U1QbtOyFInib3ZAm\"}, \"430365a65eAC8626AFf329e5B3a549f6C13D0c26\", 28161966:nat64)"
+dfx canister call proxy add_chain "(80001:nat32, vec{\"https://polygon-mumbai.g.alchemy.com/v2/0QCHDmgIEFRV48r1U1QbtOyFInib3ZAm\"}, \"06D49B4e58ef6DB855FF209454Ac6E5cB62b64ba\", 28370114:nat64)"
 
 dfx canister call proxy set_canister_addrs
 
