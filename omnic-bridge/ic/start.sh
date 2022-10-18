9c6dc7720f9c0ef1c5ee9d21470753d325caccda
ccyanxyz <ccyanxyz@gmail.com>
2022-10-18 13:56:27 +0800

    redeploy

--- omnic-bridge/ic/start.sh
+++ omnic-bridge/ic/start.sh
@@ -7,8 +7,8 @@ dfx canister --network ic call bridge set_canister_addr
 
 dfx canister --network ic call bridge set_omnic "(principal \"$(dfx canister --network ic id proxy)\")"
 
-dfx canister --network ic call bridge add_chain "(5:nat32, \"12F12F6917804c72FED66E118f99a78074F1BdE4\")"
-dfx canister --network ic call bridge add_chain "(80001:nat32, \"A8D2c4f3A422B62984D04f6F3951dbE84CB52c7b\")"
+dfx canister --network ic call bridge add_chain "(5:nat32, \"71B8D5F8b2d3e60CA0A4FfFb497Fe48DD961a44C\")"
+dfx canister --network ic call bridge add_chain "(80001:nat32, \"22216796e65F3C786d853F818ca1fc0f661639C0\")"
 
 dfx canister --network ic call bridge add_chain "(0:nat32, \"\")"
 
